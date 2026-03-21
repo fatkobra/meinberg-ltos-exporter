6af6a41b2cd10f062e5bb067dcae84c3cdf57585
Raphael Seebacher <raphael@seebachers.ch>
2026-03-21 21:11:23 +0100

    feat: rename notification event metrics

--- pkg/collector/notification.go
+++ pkg/collector/notification.go
@@ -9,7 +9,7 @@ const notificationSubsystem = "notification"
 
 var eventLastTriggered = typedDesc{
 	desc: prometheus.NewDesc(
-		prometheus.BuildFQName(MetricNamespace, "", "event_last_triggered_seconds"),
+		prometheus.BuildFQName(MetricNamespace, notificationSubsystem, "last_triggered_seconds"),
 		"When an event last occurred as seconds since UNIX epoch (0 if never triggered)",
 		[]string{"host", "type", "event"},
 		nil,
